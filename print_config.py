2f208be0414050cb2e5a8b449c3ec107728336c1
Georg Wölflein <georgw7777@gmail.com>
2022-08-20 17:15:01 +0000

    Fix configs

--- print_config.py
+++ print_config.py
@@ -1,13 +1,12 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-from hoechstgan.data.split_dataset import get_dataset_splits
+import hoechstgan  # required to register interpolations
 
 
-@hydra.main(config_path="conf", config_name="train")
+@hydra.main(config_path="conf", config_name="train", version_base="1.2")
 def app(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
-    get_dataset_splits(cfg)
+    print(OmegaConf.to_yaml(OmegaConf.to_container(cfg, resolve=True)))
 
 
 if __name__ == "__main__":
